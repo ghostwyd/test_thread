6e081ee7cdd64809408b10f4429b4ed923121b61
ghostwyd <muou1986227@163.com>
2014-01-19 03:37:11 +0800

    add content to it

--- test_thread.cpp
+++ test_thread.cpp
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/types.h>
+#include <time.h>
 #include <queue>
 
 using namespace std;
 
 #define BUFFER_SIZE       128
-#define THREAD_COUNT      8
+#define THD_COUNT         8
 
+bool      stop;
 typedef struct master_info {
     unsigned     ip;
     int          port;
@@ -16,9 +18,26 @@ typedef struct master_info {
     char         second_business[BUFFER_SIZE];
 };
 
+void* work_thread(void *ptr)
+{
+
+}
+
 
 int main(void)
 {
-    std::queue
+    std::queue<master_info>   info_list[THD_COUNT];
+    pthread_t                 thread_list[THD_COUNT];
+    stop = false;
+    srand(time(NULL));
+    int                       index = 0;
+
+    for (int i = 0; i < THD_COUNT; ++i) {
+         pthread_create(&thread_list[i], NULL, work_thread, &info_list[i]);
+    }
+    for (int i = 0; i < 160; ++i) {
+       index = random() % THD_COUNT;
+       info_list[i].;
+    }
     return EXIT_SUCCESS;
 }

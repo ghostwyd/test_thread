bd274e83fd69008e8a4dd49b5f9bf8da3a9ca1f3
ghostwyd <muou1986227@163.com>
2014-03-08 19:49:59 +0800

    add new features for test.go

--- test.go
+++ test.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
+    "net/http"
 )
 
+var g_set map[string]int
+
 func get_out(c chan string, i int) {
-	str := fmt.Sprintf("i am talking to %d\n", i)
-	time.Sleep(())
+    k := rand.Int() % 10
+    time.Sleep((time.Duration)(k) * time.Millisecond)
+	str := fmt.Sprintf("i am talking to %d.  timestamp: %d", i, k)
 	c <- str
+}
+
+func get_url(url string) {
+    rsp, err :=  http.Get("http://www.myddm.com")
+    if  ! err {
+        fmt.Printf("get %s fail!\n", url);
+    }
+    return
 }
 
 func main() {
 	var c chan string
 	c = make(chan string)
+    g_set = make(map[string]int)
+    g_set["test"] = 0
 	for i := 0; i < 10; i++ {
 		go get_out(c, i)
 	}

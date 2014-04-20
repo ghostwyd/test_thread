90941606f7e247c2c9d47d754d72fb12d8043ee1
ghostwyd <muou1986227@163.com>
2014-04-20 21:03:34 +0800

    fix bug of not receving data from server

--- test_select.cpp
+++ test_select.cpp
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 
 #define THD_NUM  1200
+#define BUF_SIZE 256 
 
 int conn_server() 
 {
@@ -41,7 +42,7 @@ int conn_server()
 
 void* do_work(void *ptr)
 {
-    char  buffer[64];
+    char  buffer[256];
     int fd = conn_server();
     if (fd < 0) {
         return NULL;
@@ -90,40 +91,45 @@ int main(void)
         pthread_join(thd_list[i], NULL);
     }*/
 
-    int     fd = conn_server();
-    int     max_fd = -1;
-    fd_set  r_set;
-    fd_set  w_set;
-    fd_set  r_tmp_set;
-    fd_set  w_tmp_set;
-    int      n = 0;
-    char     buffer[64];
-    struct timeval   timeout;
-    int              fd_list[FD_SETSIZE];
+    int                fd = -1;
+    int                max_fd = -1;
+    fd_set             r_set;
+    fd_set             w_set;
+    fd_set             r_tmp_set;
+    fd_set             w_tmp_set;
+    char               buffer[BUF_SIZE];
+    struct timeval     timeout;
+    int                fd_list[FD_SETSIZE];
 
     for (int i =0; i <  FD_SETSIZE; i++) {
         fd_list[i] = -1; 
     }
-    FD_ZERO(&r_set);
-    FD_ZERO(&w_set);
-    FD_SET(fd, &r_set);
-    FD_SET(fd, &w_set);
     timeout.tv_sec = 0;
     timeout.tv_usec = 500;
 
-    fd_list[fd] = 1;
-    if (fd > max_fd) {
-        max_fd = fd;
+    for (int i = 0; i < 1; ++i) {
+        fd = conn_server();
+        if (fd == -1) {
+           return -1;
+        }
+        fd_list[fd] = 1;
+        if (fd > max_fd) {
+            max_fd = fd;
+        }
     }
 
-    printf("fd:%d\n", fd);
-    int spec_fd = fd;
     for ( ; ; ) {
-        r_tmp_set =  r_set;
-        w_tmp_set = w_set;
-        FD_SET(spec_fd, &r_set);
-        FD_SET(spec_fd, &w_set);
-        n =  select(max_fd + 1, &r_set, &w_set, NULL,  &timeout);
+        FD_ZERO(&r_set);
+        FD_ZERO(&w_set);
+        for (int i = 0; i <= max_fd; i++) {
+            if (fd_list[i] != 1) {
+                continue;
+            }
+            FD_SET(i, &r_set);
+            FD_SET(i, &w_set);
+        }
+        //n =  select(max_fd + 1, &r_set, &w_set, NULL,  &timeout);
+        select(max_fd + 1, &r_set, &w_set, NULL,  &timeout);
 
         for (int i = 0; i < max_fd + 1; i++) { 
             if (fd_list[i] == -1) {
@@ -131,32 +137,36 @@ int main(void)
             }
             fd = i;
             if (FD_ISSET(fd, &r_set)) {
-                printf(" enter read fd:%d\n", fd);
-                int r_len = read(fd,buffer, 64);
+                printf("enter read fd:%d\n", fd);
+                int r_len = read(fd,buffer, BUF_SIZE);
                 if (r_len == 0) {
                     fprintf(stderr, "close socket\n");
                     FD_CLR(fd, &r_tmp_set);
                     close(fd);
                     return -1; 
                 } else if (r_len < 0) {
-                    if (errno == EAGAIN || errno == EINTR) {
-                        continue;
+                    if (! (errno == EAGAIN || errno == EINTR)) {
+                        fprintf(stderr, "write error:%s\n", strerror(errno));
+                        FD_CLR(fd, &r_tmp_set);
+                        close(fd);
+                        return -1;
                     }
-                    fprintf(stderr, "write error:%s\n", strerror(errno));
-                    FD_CLR(fd, &r_tmp_set);
-                    close(fd);
-                    return -1;
                 }
+                buffer[r_len]  = '\0';
+                fprintf(stderr, "fd %d receive len:%d\n", fd, r_len);
+                fprintf(stderr, "fd %d receive from server:%s\n",fd,buffer);
             } else {
-                fprintf(stderr, "not readable\n");
+                fprintf(stderr, "fd :%d, not readable\n", fd);
             }
 
             srand(time(NULL));
             if (FD_ISSET(fd, &w_set)) {
-                for (int j = 0; j <  64 ; j++) {
+                for (int j = 0; j < BUF_SIZE; j++) {
                     buffer[j] = rand() % 26 + 'A';
                 }
-                int w_len = write(fd, buffer, 64);
+                buffer[BUF_SIZE-1]= '\0';
+                int w_len = write(fd, buffer, BUF_SIZE);
+                fprintf(stderr, "send to server:%s\n", buffer);
                 if (w_len < 0) {
                     if (errno == EAGAIN || errno == EINTR) {
                         continue;
@@ -168,28 +178,11 @@ int main(void)
                 }
                 fprintf(stderr, "write len:%d\n", w_len);
                 //-------------------------------
-                int r_len = read(fd,buffer, 64);
-                if (r_len == 0) {
-                    fprintf(stderr, "close socket\n");
-                    FD_CLR(fd, &r_tmp_set);
-                    close(fd);
-                    return -1; 
-                } else if (r_len < 0) {
-                    if (errno == EAGAIN || errno == EINTR) {
-                        continue;
-                    }
-                    fprintf(stderr, "write error:%s\n", strerror(errno));
-                    FD_CLR(fd, &r_tmp_set);
-                    close(fd);
-                    return -1;
-                }
-
+            } else {
+                fprintf(stderr, "not ready for write !\n");
             }
         }
-        r_set = r_tmp_set;
-        w_set = w_tmp_set;
+        //break;
     }
-
     return EXIT_SUCCESS;
 }
-
